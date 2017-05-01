a00158df45b21da9a77ffb47e7580145db4a565b
anguyener <anguyen4@udallas.edu>
2017-05-01 11:50:25 -0500

    Added Doctor and Companion classes

---
+++ src/whovianPoetry/Companion.java
@@ -0,0 +1,34 @@
+package whovianPoetry;
+
+public class Companion {
+	
+	String badWolf(){
+
+		return "BAD WOLF";
+	}
+	String earthWalker(){
+
+		return "Martha Jones";
+	}
+	String mostImportantHuman(){
+
+		return "DoctorDona<3";
+	}
+	String girlWhoWaited(){
+
+		return "come along Pond";
+	}
+	String lastCenturion(){
+
+		return "um... cool";
+	}
+	String souffleGirl(){
+
+		return "Oswin";
+	}
+	String melody(){
+
+		return "Hello, Sweetie";
+	}
+
+}
---
+++ src/whovianPoetry/Doctor.java
@@ -0,0 +1,22 @@
+package whovianPoetry;
+
+public class Doctor {
+
+	String nine(){
+
+		return "Fantastic";
+	}
+	String ten(){
+
+		return "Allonsy";
+	}
+	String eleven(){
+
+		return "Geronimo";
+	}
+	String twelve(){
+
+		return "Listen";
+	}
+
+}
