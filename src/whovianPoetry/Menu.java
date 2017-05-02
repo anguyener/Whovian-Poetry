e3a522c2325836eeeecf8c8a06087edfe3461ea0
anguyener <anguyen4@udallas.edu>
2017-05-01 21:30:38 -0500

    Updated menu, added villain class

--- src/whovianPoetry/Menu.java
+++ src/whovianPoetry/Menu.java
@@ -6,7 +6,7 @@ public class Menu {
 
 	static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
-		
+
 		System.out.println("Who would you like your whovian poem to be about?");
 		System.out.println("1. The Doctor");
 		System.out.println("2. A Companion");
@@ -24,42 +24,41 @@ public class Menu {
 		case "3":		
 			villainMenu();
 			break;
-		case "4":
-			otherMenu();
-			break;
 		default:
-			otherMenu();
+			doctorMenu();
 		}
-
-
 	}
 
 	private static void doctorMenu() {
+		Doctor who = new Doctor();
+
 		System.out.println("Which Doctor would you like your poem to be about?");
 		System.out.println("9, 10, 11, or 12?");
-		
+
 		String choiceDr = scan.nextLine();
 
 		switch(choiceDr){
 		case "9":		
-			fantasticPoem();
+			who.nine();
 			break;
 		case "10":
-			allonsyPoem();
+			who.ten();
 			break;
 		case "11":		
-			geronimoPoem();
+			who.eleven();
 			break;
 		case "12":
-			facePoem();
+			who.twelve();
 			break;
 		default:
-			fantasticPoem();
+			who.nine();
 		}
 
 	}
 
 	private static void companionMenu() {
+		Companion run = new Companion();
+
 		System.out.println("Which companion would you like your poem to be about?");
 		System.out.println("1. Rose Tyler");
 		System.out.println("2. Martha Jones");
@@ -68,44 +67,69 @@ public class Menu {
 		System.out.println("5. Rory Williams");
 		System.out.println("6. Clara Oswald");
 		System.out.println("7. River Song");
-		
+
 		String choiceDr = scan.nextLine();
 
 		switch(choiceDr){
 		case "1":		
-			badWolfPoem();
+			run.badWolf();
 			break;
 		case "2":
-			earthWalkerPoem();
+			run.earthWalker();
 			break;
 		case "3":		
-			mostImportantHumanPoem();
+			run.mostImportantHuman();
 			break;
 		case "4":
-			girlWhoWaitedPoem();
+			run.girlWhoWaited();
 			break;
 		case "5":
-			lastCenturionPoem();
+			run.lastCenturion();
 			break;
 		case "6":
-			souffleGirlPoem();
+			run.souffleGirl();
 			break;
 		case "7":
-			melodyPoem();
+			run.melody();
 			break;
 		default:
-			badWolfPoem();
+			run.badWolf();
 		}
 
 	}
 
 
 	private static void villainMenu() {
-		// TODO Auto-generated method stub
+		Villain vil = new Villain();
 
-	}
-	private static void otherMenu() {
-		// TODO Auto-generated method stub
+		System.out.println("What villain would you like your poem to be about?");
+		System.out.println("1. Daleks");
+		System.out.println("2. Cybermen");
+		System.out.println("3. The Master");
+		System.out.println("4. Weeping Angels");
+		System.out.println("5. The Silence");
+
+		String choiceVil = scan.nextLine();
+
+		switch(choiceVil){
+		case "1":		
+			vil.daleks();
+			break;
+		case "2":
+			vil.cybermen();
+			break;
+		case "3":		
+			vil.master();
+			break;
+		case "4":
+			vil.angels();
+			break;
+		case "5":
+			vil.silence();
+			break;
+		default:
+			vil.daleks();
+		}
 
 	}
 }
---
+++ src/whovianPoetry/Villain.java
@@ -0,0 +1,27 @@
+package whovianPoetry;
+
+public class Villain {
+	
+	String daleks(){
+
+		return "EXTERMINATE";
+	}
+	String cybermen(){
+
+		return "Upgraded";
+	}
+	String master(){
+
+		return "1 2 3 4";
+	}
+	String angels(){
+
+		return "DONTBLINK";
+	}
+	String silence(){
+
+		return "silence will fall";
+	}
+
+}
+
